0ccc738a914b7bf0235e96683a9bf079eea1201c
Eugene Buslaev <eugene_buslaev@tutamail.com>
2026-02-11 22:26:38 +0300

    migrations

--- server/src/db/migrations/2 - 20260203070703-create-recipe.js
+++ server/src/db/migrations/2 - 20260203070703-create-recipe.js
@@ -1,8 +1,8 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Tasks', {
+    await queryInterface.createTable("Recipes", {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -13,30 +13,42 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false,
       },
-      text: {
-        type: Sequelize.STRING,
+      description: {
+        type: Sequelize.TEXT,
         allowNull: false,
+      },
+      image: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      cooking_time: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+      },
+      ingredients_count: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
       },
       user_id: {
         type: Sequelize.INTEGER,
         references: {
-          model: 'Users',
-          key: 'id',
+          model: "Users",
+          key: "id",
         },
       },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
-        defaultValue: Sequelize.fn('NOW'),
+        defaultValue: Sequelize.fn("NOW"),
       },
       updatedAt: {
         allowNull: false,
         type: Sequelize.DATE,
-        defaultValue: Sequelize.fn('NOW'),
+        defaultValue: Sequelize.fn("NOW"),
       },
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Tasks');
+    await queryInterface.dropTable("Recipes");
   },
 };
