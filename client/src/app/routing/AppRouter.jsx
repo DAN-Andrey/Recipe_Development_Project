38cb53fa14c3e6067a154a03f9735bbbc2390b3c
DAN-Andrey <andrey_nirvana@mail.ru>
2026-02-12 22:52:39 +0300

    полуночьный бред

--- client/src/app/routing/AppRouter.jsx
+++ client/src/app/routing/AppRouter.jsx
@@ -2,10 +2,8 @@ import { Routes, Route } from 'react-router';
 import Layout from '../Layout/Layout';
 import {
   MainPage,
-  CounterPage,
-  TasksPage,
-  TimerPage,
-  OneTaskPage,
+  RecipesPage,
+  OneRecipePage,
   AuthPage,
 } from '../../pages';
 import { CLIENT_ROUTES } from '../../shared/consts/clientRoutes';
@@ -15,8 +13,8 @@ export default function AppRouter({ setUser, user }) {
     <Routes>
       <Route path="/" element={<Layout user={user} setUser={setUser} />}>
         <Route index path={CLIENT_ROUTES.MAIN_PAGE} element={<MainPage />} />
-        <Route path={CLIENT_ROUTES.TASKS} element={<TasksPage user={user} />} />
-        <Route path={CLIENT_ROUTES.TASKS + '/:id'} element={<OneTaskPage />} />
+        <Route path={CLIENT_ROUTES.TASKS} element={<RecipesPage user={user} />} />
+        <Route path={CLIENT_ROUTES.TASKS + '/:id'} element={<OneRecipePage />} />
         <Route
           path={CLIENT_ROUTES.AUTH}
           element={<AuthPage setUser={setUser} />}
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,11 +1,9 @@
-import CounterPage from './CounterPage/CounterPage';
-import MainPage from './MainPage/MainPage';
-import TasksPage from './TasksPage/TasksPage';
-import TimerPage from './TimerPage/TimerPage';
-import OneTaskPage from './OneTaskPage/OneTaskPage';
-import AuthPage from './AuthPage/AuthPage';
+import MainPage from "./MainPage/MainPage";
+import AuthPage from "./AuthPage/AuthPage";
+import RecipesPage from "./RecipesPage/RecipesPage";
+import OneRecipePage from "./OneRecipePage/OneRecipePage";
 
 // файл с названием index запускается при обращении к папке, в которой он лежит.
 // Обычно используются для реэкспорта - для более удобной группировки файлов и сокращения импортов
 
-export { CounterPage, MainPage, TasksPage, TimerPage, OneTaskPage, AuthPage };
+export { MainPage, RecipesPage, OneRecipePage, AuthPage };
