fe255440aee350b7c34434eb562c067d1b538613
Eugene Buslaev <eugene_buslaev@tutamail.com>
2026-02-13 05:54:21 +0300

    app_router_and_header

--- client/src/app/routing/AppRouter.jsx
+++ client/src/app/routing/AppRouter.jsx
@@ -1,22 +1,21 @@
-import { Routes, Route } from 'react-router';
-import Layout from '../Layout/Layout';
-import {
-  MainPage,
-  CounterPage,
-  TasksPage,
-  TimerPage,
-  OneTaskPage,
-  AuthPage,
-} from '../../pages';
-import { CLIENT_ROUTES } from '../../shared/consts/clientRoutes';
+import { Routes, Route } from "react-router";
+import Layout from "../Layout/Layout";
+import { MainPage, RecipesPage, OneRecipePage, AuthPage } from "../../pages";
+import { CLIENT_ROUTES } from "../../shared/consts/clientRoutes";
 
 export default function AppRouter({ setUser, user }) {
   return (
     <Routes>
       <Route path="/" element={<Layout user={user} setUser={setUser} />}>
         <Route index path={CLIENT_ROUTES.MAIN_PAGE} element={<MainPage />} />
-        <Route path={CLIENT_ROUTES.TASKS} element={<TasksPage user={user} />} />
-        <Route path={CLIENT_ROUTES.TASKS + '/:id'} element={<OneTaskPage />} />
+        <Route
+          path={CLIENT_ROUTES.RECIPES}
+          element={<RecipesPage user={user} />}
+        />
+        <Route
+          path={`${CLIENT_ROUTES.RECIPES}/:id`}
+          element={<OneRecipePage user={user} />}
+        />
         <Route
           path={CLIENT_ROUTES.AUTH}
           element={<AuthPage setUser={setUser} />}
--- client/src/widgets/Header/Header.jsx
+++ client/src/widgets/Header/Header.jsx
@@ -1,38 +1,44 @@
-import { NavLink, Link } from 'react-router';
-import './Header.css';
-import UserApi from '../../entities/user/api/UserApi';
-import { setAccessToken } from '../../shared/lib/axiosInstance';
+import { NavLink, Link } from "react-router";
+import "./Header.css";
+import UserApi from "../../entities/user/api/UserApi";
+import { setAccessToken } from "../../shared/lib/axiosInstance";
+import { CLIENT_ROUTES } from "../../shared/consts/clientRoutes";
 
 export default function Header({ user, setUser }) {
   async function handleSignOut() {
     await UserApi.signOut();
-    setAccessToken('');
+    setAccessToken("");
     setUser(null);
   }
   return (
     <header>
       <nav>
-        <NavLink to="/" className="navlink">
+        <NavLink to={CLIENT_ROUTES.MAIN_PAGE} className="navlink">
           Главная
         </NavLink>
-        <NavLink to="/tasks" className="navlink">
-          Задачи
+
+        <NavLink to={CLIENT_ROUTES.RECIPES} className="navlink">
+          Рецепты
         </NavLink>
-        <NavLink to="/counter" className="navlink">
-          Счетчики
-        </NavLink>
-        <NavLink to="/timer" className="navlink">
-          Таймер
-        </NavLink>
+
+        {user && (
+          <NavLink to="/favorites" className="navlink">
+            Избранное
+          </NavLink>
+        )}
         {user ? (
           <>
-            <p>{user.username}</p>
-            <NavLink to="/auth" className="navlink" onClick={handleSignOut}>
+            <span className="username">{user.username}</span>
+            <NavLink
+              to={CLIENT_ROUTES.AUTH}
+              className="navlink"
+              onClick={handleSignOut}
+            >
               Выход
             </NavLink>
           </>
         ) : (
-          <NavLink to="/auth" className="navlink">
+          <NavLink to={CLIENT_ROUTES.AUTH} className="navlink">
             Вход
           </NavLink>
         )}
